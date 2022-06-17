8522c2d85baae6a6c125ca57df8f4f0a5291fff0
Irsan-123 <72791758+Irsan-123@users.noreply.github.com>
2022-06-17 15:29:26 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+// Import Halaman Home
+import Home from "./pages/Home";
+// Import Routes dan Route dari React Router.
+import { Route, Routes } from "react-router-dom";
+import CreateMovie from "./pages/movie/Create";
+import PopularMovie from "./pages/movie/Popular";
+import NowPlayingMovie from "./pages/movie/NowPlaying";
+import TopRatedMovie from "./pages/movie/TopRated";
+import Layout from "./Layout";
+import { ThemeProvider } from "styled-components";
+import theme from "./utils/constants/theme";
+import GlobalStyle from "./components/GlobalStyle";
+import Detail from "./pages/movie/Detail";
+
+function App() {
+
+  return (
+    <>
+
+      {/* Bungkus App dengan Theme Styled Component */}
+      <ThemeProvider theme={theme}>
+        {/* Menggunakan Global Style Component */}
+        <GlobalStyle />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/create" element={<CreateMovie />} />
+            <Route path="/movie/popular" element={<PopularMovie />} />
+            <Route path="/movie/now" element={<NowPlayingMovie />} />
+            <Route path="/movie/top" element={<TopRatedMovie />} />
+            <Route path="/movie/:id" element={<Detail />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
